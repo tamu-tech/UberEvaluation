7fda169cd4dd7c79748af98ddc0896a55bddbae3
Ubuntu <ubuntu@ip-172-31-18-163.ap-northeast-1.compute.internal>
2021-09-22 07:15:44 +0000

    precompletebootstrap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 before_action :set_q, only: [:index, :search]
 
   def index
-        @posts = Post.all.page(params[:page]).per(6).order("id DESC")
+        @posts = Post.all.page(params[:page]).per(10).order("id DESC")
         @post = Post.new
   end
   
