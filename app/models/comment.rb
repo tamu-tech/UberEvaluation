bf1fa2b68bc65c7c732a8dd83bce4390f0a15732
Ubuntu <ubuntu@ip-172-31-18-163.ap-northeast-1.compute.internal>
2021-10-04 08:44:52 +0000

    k

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   validates :content, presence: true, length: { maximum: 200 }
+  validates :username, presence: true
   belongs_to :user, optional: true
   belongs_to :post, optional: true
 end
