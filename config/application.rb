b03a2c2f1352ea0837be5a64f9d97397fc439ab2
Ubuntu <ubuntu@ip-172-31-18-163.ap-northeast-1.compute.internal>
2021-10-08 07:50:32 +0000

    completevalidation

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module UberEvaluation
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.i18n.default_locale = :ja 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
