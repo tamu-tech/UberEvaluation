642500f760c94a7847f6e911d20d9090eb879751
Ubuntu <ubuntu@ip-172-31-18-163.ap-northeast-1.compute.internal>
2021-10-08 00:43:18 +0000

    f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :user, optional: true
+    belongs_to :user
     validates :shopname, presence: true, length: { maximum: 20 }
     validates :shopaddress, presence: true, length: { maximum: 30 }
     validates :shopcontent, presence: true, length: { maximum: 300 }
