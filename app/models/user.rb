6c55e7c74a431d1c82fc181ea6a4998676bd8d4c
Ubuntu <ubuntu@ip-172-31-18-163.ap-northeast-1.compute.internal>
2021-10-10 05:55:41 +0000

    errordelete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
+
     has_many :posts
     has_many :comments
     validates :name, presence: true, length: { maximum: 20 },uniqueness: true
+
 end
